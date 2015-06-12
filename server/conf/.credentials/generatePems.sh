3d676982d97ac26756f1d4460eb99dbbb1fa1aef
Zane McCaig <zane@instanews.com>
2015-06-12 16:52:29 -0400

    Fixed circle.yml and cleaned up the conf directory a bit

--- server/conf/.credentials/generatePems.sh
+++
@@ -1,2 +0,0 @@
-openssl pkcs12 -in ./unencrypted/Certificates.p12 -out ./unencrypted/apnsCertDev.pem -nodes -clcerts
-openssl pkcs12 -in ./unencrypted/Certificates.p12 -out ./unencrypted/apnsKeyDev.pem -nocerts -nodes
