263fe9881d281321e041a0d83d39e56d6160fdfd
Zane McCaig <zane@instanews.com>
2015-06-12 17:06:00 -0400

    Fixed pushSetup.js

--- server/pushSetup.js
+++ server/pushSetup.js
@@ -1,67 +1,66 @@
 
-
 module.exports = function (app) {
-   
-   if ( process.env.NODE_ENV === 'production' ||
-     process.env.NODE_ENV === 'staging' ) 
-   {
-	  var cred = require('./conf/credentials');
-	  // Setup push notifications
-	  var App = app.models.app;
-	  var Push = app.models.push;
-	  var Votes = app.models.votes;
+  // Setup push notifications
+  var App = app.models.app;
+  var Push = app.models.push;
+  var Votes = app.models.votes;
+  var appName = 'instanews';
 
-	  var appName = 'instanews';
-	  var gcmServerApiKey = cred.gcmServerApiKey; 
-	  var apnsCertData = cred.apnsCertData;
-	  var apnsKeyData = cred.apnsKeyData;
+  if ( process.env.NODE_ENV === 'production' ||
+	 process.env.NODE_ENV === 'staging' ) 
+  {
+	 var cred = require('./conf/credentials');
 
-	  function startPushServer() {
+	 var gcmServerApiKey = cred.gcmServerApiKey; 
+	 var apnsCertData = cred.apnsCertData;
+	 var apnsKeyData = cred.apnsKeyData;
 
-		  Push.on('error', function (err) {
-			  console.log('Push Notification error: ', err.stack);
-		  });
+	 startPushServer();
+  }
+
+  function startPushServer() {
+
+	 Push.on('error', function (err) {
+		 console.log('Push Notification error: ', err.stack);
+	 });
 
 
-		  App.observe('before save', function(ctx, next) {
-			  var inst = ctx.instance;
-			  if( inst) {
-				  if( inst.name === appName) {
-					  inst.id = 'instanews';
-				  }
-			  }
-			  next();
-		  });
+	 App.observe('before save', function(ctx, next) {
+		 var inst = ctx.instance;
+		 if( inst) {
+			 if( inst.name === appName) {
+				 inst.id = 'instanews';
+			 }
+		 }
+		 next();
+	 });
 
-		  App.register('zanemcca', appName,{
-			  description: 'Local Citizen Journalism',
-			  pushSettings: {
-				  apns: {
-					  certData: apnsCertData,
-					  keyData: apnsKeyData,
-					  pushOptions: {
-						  //Could add aditional options in here
-					  },
-					  feedbackOptions: {
-						  batchFeedback: true,
-						  interval: 300
-					  }
-				  },
-				  gcm: {
-					  serverApiKey: gcmServerApiKey
-				  }
-			  }
-		  },function(err, app) {
-			  if (err) {
-				  console.log('Error Registering app: ' , err);
-			  }
-	  /*      else {
-				  console.log('Registration of app successful: ' , app);
-			  }
-			  */
-		  });
-	  }
-
-	  startPushServer();
-	}
+	 App.register('zanemcca', appName,{
+		 description: 'Local Citizen Journalism',
+		 pushSettings: {
+			 apns: {
+				 certData: apnsCertData,
+				 keyData: apnsKeyData,
+				 pushOptions: {
+					 //Could add aditional options in here
+				 },
+				 feedbackOptions: {
+					 batchFeedback: true,
+					 interval: 300
+				 }
+			 },
+			 gcm: {
+				 serverApiKey: gcmServerApiKey
+			 }
+		 }
+	 },function(err, app) {
+		 if (err) {
+			 console.log('Error Registering app: ' , err);
+		 }
+  /*      else {
+			 console.log('Registration of app successful: ' , app);
+		 }
+		 */
+	 });
+  }
 };
