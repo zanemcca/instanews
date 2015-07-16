4a2e48b477dc6fd1f8b97475caee6b713d39d914
Zane McCaig <zane@instanews.com>
2015-07-15 22:28:00 -0400

    Fix the broken test and cleaned up sample-data

--- test/sample-data/import.js
+++ test/sample-data/import.js
@@ -1,191 +1,86 @@
-var async = require('async');
 
-var journalists = require('./journalist.json');
-var comments = require('./comment.json');
-var articles = require('./article.json');
-var subarticles = require('./subarticle.json');
-//var roles = require('./role.json');
+var app = require('../../server/server'); 
 
-module.exports = function (app, cb) {
+var Articles = app.models.Article;
+var Subarticles = app.models.Subarticle;
+var UpVotes = app.models.upVote;
+var DownVotes = app.models.downVote;
 
-   var Articles = app.models.Article;
-   var Subarticles = app.models.Subarticle;
-   var Journalists = app.models.Journalist;
-   var Comments    = app.models.Comment;
-   var Roles =       app.models.Role;
-   var RoleMapping = app.models.RoleMapping;
-   var AccessToken = app.models.AccessToken;
+function randomLat() {
+   max = 46;
+   min = 45;
+   return Math.random()*(max - min) + min;
+}
 
-   var UpVotes = app.models.upVote;
-   var DownVotes = app.models.downVote;
+function randomLng() {
+   max = -74;
+   min = -73;
+   return Math.random()*(max - min) + min;
+}
 
-   var ArticlesDB = app.datasources.Articles;
-   var UserDB = app.datasources.Users;
-   var db = app.datasources.db;
+function randomId() {
+   var id = Math.floor(Math.random()*Math.pow(2,32));
+   return id;
+}
 
-   /*
-   var st = app.datasources.photos;
-   var container = st.createModel('container');
-   app.model(container);
-   */
+function callback(err, res) {
+   if( err) {
+      console.log('Erorr: ' + err);
+   }
+   //console.log(res);
+}
 
-   // This function imports all members of the data array into the given Model
-   function importData(Model, data, callb) {
+var fillCount = 0;
+var total = 0;
+var limit = 200;
 
-      console.log('Importing model - ', Model.modelName);
+function fillArticle(err, res) {
+   fillCount++;
+   total++;
+   Subarticles.create({
+      parentId: res.id,
+      username: 'bob',
+      _file: {
+         type: 'image',
+         size: 2000,
+         name: '123456.jpg',
+         caption: 'Nuts photo!!'
+      }
+   }, callback);
 
-      //Clear all existing things in the model
-      Model.destroyAll( function(err) {
-         if (err) return callb(err);
 
-         if (data.length > 0) {
-            console.log('Populating...');
-            //Create a model instance for each member of the array
-            async.each(data, function (d, callback) {
-               Model.create(d, callback);
-            }, function(err, res) {
-               if(err) return callb(err);
-
-               callb(null, Model.modelName);
-            });
-         }
-         else {
-            callb(null);
-         }
-      });
+   //console.log('Voting on ' + res.id);
+   var max = Math.floor(Math.random()*100);
+   for( var i =0; i < max; i++) {
+      DownVotes.create({
+         votableId: res.id,
+         votableType: 'article'
+      }, callback);
    }
 
-   function importArticles(cb) {
-
-      function randomLat() {
-         max = 46;
-         min = 45;
-         return Math.random()*(max - min) + min;
-      }
-
-      function randomLng() {
-         max = -65.5;
-         min = -67;
-         return Math.random()*(max - min) + min;
-      }
-
-      function randomId() {
-         var id = Math.floor(Math.random()*Math.pow(2,32));
-         return id;
-      }
-
-      function callback(err, res) {
-         if( err) {
-            console.log('Erorr: ' + err);
-         }
-         //console.log(res);
-      }
-
-      function fillArticle(err, res) {
-         Subarticles.create({
-            parentId: res.id,
-            username: 'bob',
-            _file: {
-               type: 'image',
-               size: 2000,
-               name: '123456.jpg',
-               caption: 'Nuts photo!!'
-            }
-         }, callback);
-
-
-         //console.log('Voting on ' + res.id);
-         var max = Math.floor(Math.random()*100);
-         for( var i =0; i < max; i++) {
-            DownVotes.create({
-               votableId: res.id,
-               votableType: 'article'
-            }, callback);
-         }
-
-         max = Math.floor(Math.random()*20);
-         for(i =0; i < max; i++) {
-            UpVotes.create({
-               votableId: res.id,
-               votableType: 'article'
-            }, callback);
-         }
-      }
-
-      var limit = 200;
-
-      for( var i = 0; i < limit ; i++ ) {
-         Articles.create({
-            title: i,
-            location: {
-               lat: randomLat(),
-               lng: randomLng()
-            },
-            isPrivate: false
-         }, fillArticle);
-      }
-      cb(null);
-
+   max = Math.floor(Math.random()*20);
+   for(i =0; i < max; i++) {
+      UpVotes.create({
+         votableId: res.id,
+         votableType: 'article'
+      }, callback);
    }
-
-   function createAdmin(callback) {
-      AccessToken.destroyAll( function(err) {
-         if (err) return callback(err);
-
-         RoleMapping.destroyAll( function(err) {
-            if (err) return callback(err);
-
-            Roles.destroyAll( function(err) {
-               if (err) return callback(err);
-
-               Roles.create({
-                  name: 'admin'
-               }, function(err, role) {
-                  if (err) return callback(err);
-                  console.log('Created role:', role.name);
-
-                  role.principals.create({
-                     principalType: RoleMapping.USER,
-                     principalId: journalists[0].username
-                  }, function(err, principal) {
-                     if (err) return callback(err);
-
-                     console.log('Created principal:', principal.principalType);
-                     callback();
-                  });
-               });
-            });
-         });
-      });
+   fillCount--;
+   if(total === limit) {
+     process.exit(1);
    }
+}
 
-   function updateDB(callback) {
-      ArticlesDB.autoupdate(
-            UserDB.autoupdate(
-               db.autoupdate( function(err, res) {
-                  if (err) console.log('db Error');
-                  callback(err, res);
-               })));
-   }
+console.log('Starting Import');
+for( var i = 0; i < limit ; i++ ) {
+   Articles.create({
+      title: i,
+      location: {
+         lat: randomLat(),
+         lng: randomLng()
+      },
+      isPrivate: false
+   }, fillArticle);
+}
 
-   console.log('Starting the import of sample data');
-
-   async.series([
-      importData.bind(null,Journalists, journalists),
-      importData.bind(null,Articles, articles),
-      importData.bind(null,Subarticles, subarticles),
-      importData.bind(null,UpVotes, []),
-      importData.bind(null,DownVotes, []),
-      importData.bind(null,Comments, comments),
-      createAdmin.bind(null),
-      importArticles.bind(null),
-      updateDB.bind(null)
-   ], function(err,res) {
-         if (err) {
-            console.log('\nFailed to import the data!\n', err);
-         }
-         else console.log('Completed the sample data importing');
-
-         cb(err);
-   });
-};
+console.log('Done Importing!');
