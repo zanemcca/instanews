25cbf91a584e89bba88148cfdb21d811e57f7199
Zane McCaig <zane@instanews.com>
2015-11-22 21:08:19 -0500

    Created a textInput service and a textFooter directive

--- client/app/scripts/directives/comments.js
+++ client/app/scripts/directives/comments.js
@@ -3,20 +3,22 @@
 var app = angular.module('instanews.directive.comments', ['ionic', 'ngResource']);
 
 app.directive('incomments', [
-      'Comment',
-      'Subarticle',
-      'Article',
-      function (
-        Comment,
-        Subarticle,
-        Article
-      ) {
+  'Comment',
+  'Subarticle',
+  'TextInput',
+  'Article',
+  function (
+    Comment,
+    Subarticle,
+    TextInput,
+    Article
+  ) {
 
-   return {
+    return {
       restrict: 'E',
       scope: {
-         owner: '='
-      },
+        owner: '='
+      }, 
       controller: function($scope) {
 
         var Models = {
@@ -25,60 +27,68 @@ app.directive('incomments', [
           subarticle: Subarticle
         };
 
-         $scope.createComment = function (instance, content) {
-            Comment.create({
-               content: content,
-               commentableId: instance.id,
-               commentableType: instance.modelName
-            }).$promise
-            .then(function(res,err) {
-              // istanbul ignore if 
-               if(err) {
-                  console.log(err);
-               }
-               else {
-                  instance.comments.push(res);
-               }
-            });
-         };
+        $scope.create = function () {
+          TextInput.get().open(function (text) {
+            createComment(text);
+          });
+        };
 
-         $scope.loadMore = function(instance) {
-            var order = 'rating DESC';
-            if ( instance.commentableId ) {
-               order = 'date DESC';
-            }
-
-            // istanbul ignore else 
-            if(Models.hasOwnProperty(instance.modelName)) {
-              Models[instance.modelName].comments({
-                 id: instance.id,
-                 filter: {
-                    limit: 10,
-                    skip: instance.comments.length,
-                    order: order
-                 }
-              }).$promise
-              .then( function (comments) {
-                 //Remove duplicates
-                 for( var i = 0; i < comments.length; i++) {
-                    for( var j = 0; j < instance.comments.length; j++) {
-                       var comment = instance.comments[j];
-                       if( comment.id === comments[i].id) {
-                          comments.splice(i,1);
-                          break;
-                       }
-                    }
-                 }
-                 //Concatinate the new comments
-                 instance.comments = instance.comments.concat(comments);
-                 //TODO Ensure the comments are in their proper order
-              });
+        var createComment = function (content) {
+          var instance = $scope.owner;
+          Comment.create({
+            content: content,
+            commentableId: instance.id,
+            commentableType: instance.modelName
+          }).$promise
+          .then(function(res,err) {
+            // istanbul ignore if 
+            if(err) {
+              console.log(err);
             }
             else {
-              console.log('Warning: Unknown commentableType!');
+              instance.comments.push(res);
             }
-         };
+          });
+        };
+
+        //TODO MOve comments over to the list service. Similar to subarticles and articles
+        $scope.loadMore = function(instance) {
+          var order = 'rating DESC';
+          if ( instance.commentableId ) {
+            order = 'date DESC';
+          }
+
+          // istanbul ignore else 
+          if(Models.hasOwnProperty(instance.modelName)) {
+            Models[instance.modelName].comments({
+              id: instance.id,
+              filter: {
+                limit: 10,
+                skip: instance.comments.length,
+                order: order
+              }
+            }).$promise
+            .then( function (comments) {
+              //Remove duplicates
+              for( var i = 0; i < comments.length; i++) {
+                for( var j = 0; j < instance.comments.length; j++) {
+                  var comment = instance.comments[j];
+                  if( comment.id === comments[i].id) {
+                    comments.splice(i,1);
+                    break;
+                  }
+                }
+              }
+              //Concatinate the new comments
+              instance.comments = instance.comments.concat(comments);
+              //TODO Ensure the comments are in their proper order
+            });
+          }
+          else {
+            console.log('Warning: Unknown commentableType!');
+          }
+        };
       },
       templateUrl: 'templates/directives/comments.html'
-   };
-}]);
+    };
+  }]);
