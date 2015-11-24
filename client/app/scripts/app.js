2fe127805295a331360631c5affc9744fed9327a
Zane McCaig <zane@instanews.com>
2015-11-23 19:00:30 -0500

    Fixed karma issues with moment

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -85,6 +85,25 @@ angular.module('instanews', [
   $ionicConfigProvider
 ) {
 
+  // jshint undef: false
+  moment.locale('en', {
+    relativeTime: {
+      future: 'in %s',
+      past: '%s ago',
+      s: '%d sec',
+      m: 'a minute',
+      mm: '%d minutes',
+      h: 'an hour',
+      hh: '%d hours',
+      d: 'a day',
+      dd: '%d days',
+      M: 'a month',
+      MM: '%d months',
+      y: 'a year',
+      yy: '%d years'
+    }
+  }); 
+
   //No transitions for performance
   $ionicConfigProvider.views.transition('none');
 
@@ -179,22 +198,3 @@ angular.module('instanews', [
   $urlRouterProvider.otherwise('/app/feed');
 
 });
-
-// jshint undef: false
-moment.locale('en', {
-  relativeTime: {
-    future: 'in %s',
-    past: '%s ago',
-    s: '%d sec',
-    m: 'a minute',
-    mm: '%d minutes',
-    h: 'an hour',
-    hh: '%d hours',
-    d: 'a day',
-    dd: '%d days',
-    M: 'a month',
-    MM: '%d months',
-    y: 'a year',
-    yy: '%d years'
-  }
-}); 
