3c6cbbe0dc45f9301bd0a89129ebe16b7e9ca826
Zane McCaig <zane@instanews.com>
2015-12-06 15:42:37 -0500

    Cleaned up a bit and upgraded Case

--- client/app/scripts/controllers/post.js
+++ client/app/scripts/controllers/post.js
@@ -42,18 +42,7 @@ app.controller('PostCtrl', [
       if(newTitle !== oldTitle) {
         if(newTitle && newTitle.length > 0) {
           // jshint undef: false
-          var title = Case.title(newTitle);
-
-          // Capitalize the first character of the last word
-          var lastSpaceIdx = title.lastIndexOf(' ');
-          if(lastSpaceIdx > -1) {
-            var lastWord = title.substr(lastSpaceIdx + 1);
-            if(lastWord.length > 0) {
-              title = title.slice(0, lastSpaceIdx + 1) + lastWord.charAt(0).toUpperCase() + lastWord.substr(1); 
-            }
-          }
-
-          $scope.newArticle.title = title;
+          $scope.newArticle.title = Case.title(newTitle);
         }
       }
     });
@@ -186,79 +175,5 @@ app.controller('PostCtrl', [
         console.log('Cannot post without subarticles');
       }
     };
-
-    /* Text Posting */
-    /*
-    $scope.data = {
-      text: '',
-    };
-
-    //Modal for posting text
-    $ionicModal.fromTemplateUrl('templates/postTextModal.html', {
-      scope: $scope,
-      animation: 'slide-in-up'
-    }).then( function (modal) {
-      $scope.postTextModal = modal;
-    });
-
-    //Clean up the temp text value and hide the modal
-    $scope.trashText = function() {
-      $scope.data.text = '';
-      $scope.postTextModal.hide();
-    };
-
-    //Move the text out of the form so that it is ready to be submitted
-    $scope.saveText = function() {
-      $scope.uploads.unshift($scope.Upload.text($scope.data.text));
-      $scope.trashText();
-    };
-
-    //Capture video using the video camera
-    $scope.captureVideo = function() {
-      Camera.captureVideo()
-      .then( function(video) {
-        if(video) {
-          $scope.uploads.unshift($scope.Upload.video(video));
-        }
-      },
-      // istanbul ignore next
-      function(err) {
-        console.log(err);
-      });
-    };
-
-    //Get a photo(s) from the gallery
-    $scope.openMediaGallery = function() {
-      Camera.openMediaGallery()
-      .then( function(media) {
-        if(media) {
-          console.log(media);
-          if(media.type.indexOf('image') > -1) {
-            $scope.uploads.unshift($scope.Upload.picture(media));
-          } else if (media.type.indexOf('video') > -1) {
-            $scope.uploads.unshift($scope.Upload.video(media));
-          }
-        }
-      },
-      // istanbul ignore next
-      function(err) {
-        console.log(err);
-      });
-    };
-
-    //Capture a photo using the camera and store it into the new article
-    $scope.capturePicture = function() {
-      Camera.capturePicture()
-      .then( function(photo) {
-        if(photo) {
-          $scope.uploads.unshift($scope.Upload.picture(photo));
-        }
-      }, 
-      // istanbul ignore next
-      function(err) {
-        console.log('Error: Failed to capture a new photo: ' + JSON.stringify(err));
-      });
-    };
-    */
   }
 ]);
