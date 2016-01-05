56c2c906dce8e5c942c53d0b0dfe8dd075be2c79
Zane McCaig <zane@instanews.com>
2016-01-05 17:18:50 -0500

    Finished edit

--- client/app/scripts/directives/upload.js
+++ client/app/scripts/directives/upload.js
@@ -31,11 +31,52 @@ app.controller(
 // istanbul ignore next
 app.directive(
   'inupload', [
-    function () {
+    function (
+    ) {
       return {
         restrict: 'E',
         scope: {
           upload: '='
+        },
+        controller: function ($scope, TextInput) {
+          var newText = '';
+          var textInput;
+          if($scope.upload.subarticle.text) {
+            $scope.edit = function () {
+              textInput = TextInput.get('modal');
+              textInput.placeholder = 'What\'s the story?';
+              newText = newText || $scope.upload.subarticle.text;
+              textInput.text = newText;
+
+              textInput.open(function (text) {
+                $scope.upload.subarticle.text = text; 
+              }, function (partialText) {
+                //Interruption function
+                newText = partialText;
+              });
+            };
+          } else {
+            $scope.caption = {
+              edit: function () {
+                textInput = TextInput.get('modal');
+                textInput.placeholder = 'What\'s the caption?';
+                newText = newText || $scope.upload.subarticle._file.caption;
+                textInput.text = newText;
+
+                textInput.open(function (text) {
+                  $scope.upload.subarticle._file.caption = text;
+                  }, function (partialText) {
+                  //Interruption function
+                  newText = partialText;
+                });
+              }
+            };
+          }
+          $scope.is = {
+            mine: function () {
+              return !$scope.upload.isPosting;
+            }
+          };
         },
         link: function ($scope) {
           $scope.upload.resolve();
