90b6f7498b0514609286f1e474ec1e8f65e91607
Zane McCaig <zane@instanews.com>
2016-01-21 19:03:28 -0500

    Added an admin identifier to the journalist models

--- server/boot/admin.js
+++ server/boot/admin.js
@@ -1,18 +1,26 @@
+
 module.exports = function(app) {
   var Admin = app.models.Admin;
   var Role = app.models.Role;
+  var loopback = require('loopback');
 
-  Role.registerResolver('admin', function(role, context, cb) {
+  Role.registerResolver('admin', function(role, ctx, cb) {
     function reject() {
       process.nextTick(function() {
         cb(null, false);
       });
     }
 
-    // do not allow anonymous users
-    var userId = context.accessToken.userId;
-    if (!userId) {
-      return reject();
+    var userId;
+    var context = loopback.getCurrentContext();
+    if(context) {
+      var token = context.get('accessToken');
+      if(token) {
+        userId = token.userId;
+        if(!userId) {
+          return reject();
+        }
+      }
     }
 
     // Check if the user is an admin 
@@ -34,6 +42,5 @@ module.exports = function(app) {
       console.log(user);
       cb(null, true); // true = is a team member
     });
-
   });
 };
