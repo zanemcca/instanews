32c7ab66a4b98df1c49e8c8f5f021545775dbb78
Zane McCaig <zane@instanews.com>
2016-03-23 11:54:29 -0400

    Should solve #326. Hard to tell without a reliable reproduction

--- client/app/scripts/controllers/feed.js
+++ client/app/scripts/controllers/feed.js
@@ -31,6 +31,19 @@ app.controller('FeedCtrl', [
   ) {
     $scope.Articles = Articles.getLoader({
       preload: true,
+      filter: function(articles) {
+        //Ensure articles are w ithin view before adding to the list
+        //This takes care of any race conditions between the preloading queue and bounds updating
+        var arts = [];
+        for(var i in articles) {
+          var position = Position.posToLatLng(articles[i].location);
+          if(Position.withinBounds(position)) {
+            arts.push(articles[i]);
+          }
+        }
+
+        return arts;
+      },
       keepSync: true
     });
 
