57ab89d5c44cb243faa67d1a0f004a939f5e737b
Zane McCaig <zane@instanews.com>
2016-04-04 14:08:15 -0400

    Added email on unknown error code and a takedown of the offending node

--- server/middleware/errorHandler.js
+++ server/middleware/errorHandler.js
@@ -1,16 +1,8 @@
 
-/*
-var nodemailer = require('nodemailer');
-var transport = nodemailer.createTransport({
-  service: 'Gmail',
-  auth: {
-    user: 'error@instanews.com',
-    pass: 'couchesareabit2fly4me'
-  }
-});
-*/
-
 module.exports = function(options) {
+
+  var email = require('../helpers/email.js').email;
+
   return function errorHandler(err, req, res, next) {
     /* istanbul ignore if */
     if(process.env.NODE_ENV === 'production' ||
@@ -30,31 +22,33 @@ module.exports = function(options) {
 
       if(res.statusCode === 404) {
         e.message = 'This is not the page you are looking for ...';
-      }
-      else if( err.status === 401 || err.status === 403 ) {
+      } else if( err.status === 401 || err.status === 403 ) {
         e.message = 'Thou shalt not pass!!!';
       }
-        /*
       else {
         console.error('Unrecoverable - error status code!');
         console.error(err.stack);
 
-        if(process.env.NODE_ENV === 'production') {
+//        if(process.env.NODE_ENV === 'production') {
           //Send a notification to the backend manager
-          transport.sendMail({
+          //transport.sendMail({
+          email.send({
             from: 'error@instanews.com',
             to: 'alertbackend@instanews.com',
             subject: err.message,
             text: err.stack
-          }, function(erri, info) {
-            if (err) console.error(err);
-            console.log('Error report has been mailed to' +
-                        'alertbackend@instanews.com\n');
+          }, function(err, info) {
+            if (err) {
+              console.log('Failed to send email!');
+              console.error(err);
+            } else {
+              console.log('Error report has been mailed to' +
+                          'alertbackend@instanews.com\n');
+            }
             process.exit(1);
           });
-        }
+  //      }
       }
-       */
 
       res.send(e);
     }
