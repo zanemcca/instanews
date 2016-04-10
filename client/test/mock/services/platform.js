39034cb818df69f99f485cc5e0b24508ec02ae4f
Zane McCaig <zane@instanews.com>
2016-04-10 14:16:42 -0400

    Used mock directory for position service tests

--- client/test/mock/services/platform.js
+++ client/test/mock/services/platform.js
@@ -11,6 +11,18 @@ angular.module('mock.services.platform', []).service('Platform', function() {
       trackEvent: function () {},
     },
     initBackButton: function() {},
+    isBrowser: function() {},
+    isAndroid6: function () {
+      return false;
+    },
+    ready: {
+      then: function (cb) {
+        cb();
+      }
+    },
+    getUUID: function () {
+      return 'uuid';
+    },
     isIOS: function() {
       return true;
     },
